b0da4c6b3c0c623ad7dee0bf958a80576ac8c895
Leo <114874220+Brasilius@users.noreply.github.com>
2023-02-09 21:19:03 -0600

    Update main.rs

--- ArrayTest/src/main.rs
+++ ArrayTest/src/main.rs
@@ -3,8 +3,8 @@ fn main() {
     println!("Input your index value: ");
     let x = [1,2,3,4,5,6];
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("shut up bitch!");
-    let input: usize = input.trim().parse().expect("that shit aint a number");
+    io::stdin().read_line(&mut input).expect("Failure to read string.");
+    let input: usize = input.trim().parse().expect("Please input a proper number integer.");
     let y = input - 1;
     let element = x[y];
     println!("your mf value at index {input} is {element}");
