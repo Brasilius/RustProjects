4f23f65d3bf8e0f56c1be2696ee4f0bb4e14e066
niels <Niels.L.Larsen-1@ou.edu>
2023-02-13 21:49:44 -0600

    reorganized the structure of music_storage to be less messy

--- music_storage/src/main.rs
+++ music_storage/src/main.rs
@@ -1,3 +1,22 @@
+use std::fs::File;
+use std::fs::write;
 fn main() {
-    println!("Hello, world!");
+
+let music_sheet = File::create("Music_Sheet.txt").expect("failed to create file");
+let data1 = "this is a string in a text file";
+let path = "/home/niels/RustProjects/music_storage/Music_Sheet.txt";
+write(path, data1).expect("unable to write to file");
+
+
+
+
 }
+   
+   fn writemusic(){
+
+    println!("hello world!");
+
+   }
+   fn readmusic(){
+    println!("hello world from read!");
+   }
